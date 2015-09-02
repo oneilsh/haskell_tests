8ab629a786f924ab9444684982225253ac2a5165
Shawn O'Neil <oneilsh@gmail.com>
2015-09-02 08:48:25 -0700

    Made the align.hs O(mn) by using : (cons)

--- align.hs
+++ align.hs
@@ -8,15 +8,15 @@ rep xs n
 
 
 score_pair a b 
-	| length a == 1 && length b == 1 && a == " " && b == " " = 0
-    | a == "-" = -4
-    | b == "-" = -4
+	| a == ' ' && b == ' ' = 0
+    | a == '-' = -4
+    | b == '-' = -4
     | a == b = 2
     | otherwise = -3
 
 score_aln a b 
-	| length a == 1 && length b == 1 = score_pair a b
-	| otherwise = score_aln (tail a) (tail b) + score_pair [(head a)] [(head b)] -- we always work with strings :)
+	| length a == 1 && length b == 1 = score_pair (head a) (head b)
+	| otherwise = score_aln (tail a) (tail b) + score_pair (head a) (head b) 
 
 type Unalign = String
 type Align = String
@@ -51,25 +51,25 @@ max_answer a b c
 -- wooohoo recursion
 global_aln a b
 	| length a == 1 || length b == 1 = base_case a b
-	| otherwise = let {b_a = [head a];
-	                   b_b = [head b];
+	| otherwise = let {b_a = head a;
+	                   b_b = head b;
 	                   s_a = tail a;
 	                   s_b = tail b;
 	                   
-	                   left = global_aln_mem s_a (b_b ++ s_b);
+	                   left = global_aln_mem s_a b;
 	                   center = global_aln_mem s_a s_b;
-	                   right = global_aln_mem (b_a ++ s_a) s_b;
+	                   right = global_aln_mem a s_b;
 	                   
-	                   left_aln_a = b_a ++ (aaln left);
-	                   left_aln_b = "-" ++ (baln left);
-	                   center_aln_a = b_a ++ (aaln center);
-	                   center_aln_b = b_b ++ (baln center);
-	                   right_aln_a = "-" ++ (aaln right);
-	                   right_aln_b = b_b ++ (baln right);
+	                   left_aln_a = b_a : (aaln left);
+	                   left_aln_b = '-' : (baln left);
+	                   center_aln_a = b_a : (aaln center);
+	                   center_aln_b = b_b : (baln center);
+	                   right_aln_a = '-' : (aaln right);
+	                   right_aln_b = b_b : (baln right);
 	                   
-	                   answer_left = Answer   a   b   left_aln_a   left_aln_b   (score left + score_pair b_a  "-");
+	                   answer_left = Answer   a   b   left_aln_a   left_aln_b   (score left + score_pair b_a  '-');
 	                   answer_center = Answer   a   b   center_aln_a  center_aln_b  (score center + score_pair b_a b_b);
-	                   answer_right = Answer   a   b   right_aln_a  right_aln_b   (score right + score_pair "-" b_b)}
+	                   answer_right = Answer   a   b   right_aln_a  right_aln_b   (score right + score_pair '-' b_b)}
 	              in
 	                   max_answer answer_left answer_center answer_right
 
